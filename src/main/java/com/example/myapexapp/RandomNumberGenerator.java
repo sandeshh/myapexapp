6dd2962bb1d6f1863b167d1690992e83c863bdbc
Sandesh Hegde <sandesh.hegde@gmail.com>
2016-10-06 14:41:41 -0700

    changes

---
+++ src/main/java/com/example/myapexapp/RandomNumberGenerator.java
@@ -0,0 +1,49 @@
+/**
+ * Put your copyright and license info here.
+ */
+package com.example.myapexapp;
+
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.InputOperator;
+import com.datatorrent.common.util.BaseOperator;
+
+/**
+ * This is a simple operator that emits random number.
+ */
+public class RandomNumberGenerator extends BaseOperator implements InputOperator
+{
+  private int numTuples = 100;
+  private transient int count = 0;
+  private long windowId;
+
+  public final transient DefaultOutputPort<Long> out = new DefaultOutputPort<Long>();
+
+  @Override
+  public void beginWindow(long windowId)
+  {
+    this.windowId = windowId;
+    count = 0;
+  }
+
+  @Override
+  public void emitTuples()
+  {
+    if (count++ < numTuples) {
+      out.emit(windowId);
+    }
+  }
+
+  public int getNumTuples()
+  {
+    return numTuples;
+  }
+
+  /**
+   * Sets the number of tuples to be emitted every window.
+   * @param numTuples number of tuples
+   */
+  public void setNumTuples(int numTuples)
+  {
+    this.numTuples = numTuples;
+  }
+}
